de78204a5a34ab4c1dc45dba15ca0771b8b52c73
Daniel Alejandro Diaz Peraza <diazperd@shernet.sheridancollege.ca>
2026-03-31 10:50:04 -0400

    First and second challenge

---
+++ Assets/Scripts/GymScripts/Kaiju.cs
@@ -0,0 +1,60 @@
+using Unity.Cinemachine;
+using Unity.Mathematics;
+using Unity.VisualScripting;
+using UnityEditor.Tilemaps;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Kaiju : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public AudioSource SFX;
+    public ParticleSystem ParticleSystem;
+
+    public float speed = 5;
+    public Vector2 movement;
+    public Animator animator;
+    public SpriteRenderer spriteRenderer;
+
+    public CinemachineImpulseSource Shake;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += (Vector3)movement * speed * Time.deltaTime;
+        animator.SetFloat("Speed", movement.magnitude);
+        if (movement.x < 0)
+        {
+            spriteRenderer.flipX = true;
+            ParticleSystem.transform.localScale = new Vector3(-1, 1, 1);
+
+        }
+        else
+        {
+            spriteRenderer.flipX = false;
+            ParticleSystem.transform.localScale = new Vector3(1, 1, 1);
+        }
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        movement = context.ReadValue<Vector2>();
+    }
+
+    public void Footstep()
+    {
+        //Debug.Log("step");
+        SFX.Play();
+        ParticleSystem.Play();
+        Shake.GenerateImpulse();
+
+
+    }
+}
