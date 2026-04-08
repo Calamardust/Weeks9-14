c26c33585162cae90d5b78d6912d1d358c228a1f
Calamardust <diazperd@sheridancollege.ca>
2026-04-07 22:43:29 -0400

    Coment code and minor changes

--- Assets/Scripts/System Scripts/Sigths.cs
+++ Assets/Scripts/System Scripts/Sigths.cs
@@ -3,18 +3,20 @@ using UnityEngine.InputSystem;
 
 public class Sigths : MonoBehaviour
 {
+    //cooldown between shots
     public float cooldownTime;
-
     public bool cooldown;
 
+    // Speed for the sigths movement
     private float speed = 10;
 
+    //Vector for sigths movement
     public Vector2 movement;
 
     //public AudioSource SFX;
 
+    // reference for the sprite and particles
     public SpriteRenderer SpriteRenderer;
-
     public ParticleSystem ParticleSystem;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -26,18 +28,21 @@ public class Sigths : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // add movement
         transform.position += (Vector3)movement * speed * Time.deltaTime;
 
     }
+    // Input system for movement (WASD or arrow keys)
     public void OnMove(InputAction.CallbackContext context)
     {
         movement = context.ReadValue<Vector2>();
     }
+    // "Shoot" on presing space
     public void OnJump(InputAction.CallbackContext context)
     {
         
-        
-        if (context.performed && !cooldown)
+        //changes color and plays particles when pressed and the cooldown is false
+        if (context.started && !cooldown)
         {
             cooldown = true;
             //StartCoroutine()
@@ -45,6 +50,7 @@ public class Sigths : MonoBehaviour
             ParticleSystem.Play();
             
         }
+        // changes back to green
         else
         {
             SpriteRenderer.color = Color.green;
